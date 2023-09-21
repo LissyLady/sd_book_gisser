5c61d9a3159864af5fb698a743af6cf892e57456
LissyLady <64424696+LissyLady@users.noreply.github.com>
2023-09-21 22:10:49 +0200

    changed add book to dialog

--- buchverwaltungapp/src/app/components/book-list/book-list.component.ts
+++ buchverwaltungapp/src/app/components/book-list/book-list.component.ts
@@ -1,28 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { AddBookDialogComponent } from '../book-dialog/book-dialog.component';
 import { BookService } from 'src/app/services/book.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
-  books: any[] = [];
+    books: any[] = [];
+    searchText = '';
+    selectedFilter = 'all';
 
-  constructor(private bookService: BookService) {}
+    constructor(private bookService: BookService, private dialog: MatDialog) { }
 
-  ngOnInit() {
-    this.loadBooks();
-  }
+    ngOnInit(): void {
+        this.getBooks();
+    }
 
-  loadBooks() {
-    this.bookService.getBooks().subscribe((data) => {
-      this.books = data;
-    });
-  }
+    getBooks(): void {
+        this.bookService.getBooks().subscribe((books) => {
+            this.books = books;
+        });
+    }
 
-  deleteBook(id: string) {
-    this.bookService.deleteBook(id).subscribe(() => {
-      this.loadBooks();
-    });
-  }
+    openAddBookDialog(): void {
+        const dialogRef = this.dialog.open(AddBookDialogComponent, {
+            width: '400px',
+            data: {},
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result) {
+                this.bookService.createBook(result).subscribe(() => {
+                    this.getBooks();
+                });
+            }
+        });
+    }
+
+    editBook(book: any): void {
+        // Implementieren Sie die Bearbeiten-Funktion hier
+    }
+
+    deleteBook(book: any): void {
+        // Implementieren Sie die Löschen-Funktion hier
+    }
+
+    filterBooks(): any[] {
+        if (this.selectedFilter === 'all') {
+            return this.books.filter((book) =>
+                book.title.toLowerCase().includes(this.searchText.toLowerCase())
+            );
+        } else if (this.selectedFilter === 'title') {
+            return this.books.filter((book) =>
+                book.title.toLowerCase().includes(this.searchText.toLowerCase())
+            );
+        } else if (this.selectedFilter === 'isbn') {
+            return this.books.filter((book) =>
+                book.isbn.toLowerCase().includes(this.searchText.toLowerCase())
+            );
+        } else {
+            return this.books;
+        }
+    }
 }
