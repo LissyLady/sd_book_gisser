c674cf42fd823dd35bbc1e4ef3d3582d7076d979
LissyLady <64424696+LissyLady@users.noreply.github.com>
2023-09-21 21:51:57 +0200

    added book-detail component

---
+++ buchverwaltungapp/src/app/components/book-detail/book-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+})
+export class BookDetailComponent implements OnInit {
+  book: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const bookId = params.get('id');
+      this.bookService.getBookById(bookId).subscribe((data) => {
+        this.book = data;
+      });
+    });
+  }
+}
